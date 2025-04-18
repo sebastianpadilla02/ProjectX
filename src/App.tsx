a2ad910193baa1f8d3f502df4a2704100ce0c090
sebastianpadilla02 <sebastianpadilla@uninorte.edu.co>
2025-04-18 00:30:00 -0500

    implement authentication check and redirect in App component; add logout functionality in Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,37 @@ import SignUp from './components/public/signup';
 import Layout from './components/private/layout/layout';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/home" element={<Layout />} />
-        {/* Redirigir a login por defecto */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+
+  // Verificar si el usuario está autenticado
+  const isAuthenticated = localStorage.getItem('token') !== null;
+
+  if (isAuthenticated) {
+    // Redirigir a la página de home si el usuario está autenticado
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/home" element={<Layout />} />
+          {/* Redirigir a login por defecto */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  } else {
+    // Redirigir a la página de login si el usuario no está autenticado
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/home" element={<Layout />} />
+          {/* Redirigir a login por defecto */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
